bc8b9754d6c25ddf693369843589dcbee725c6a0
Stewart Bell <stewbell@gmail.com>
2020-09-16 19:37:54 +1000

    call cloud function working

--- Javascript/data.js
+++ Javascript/data.js
@@ -34,25 +34,31 @@ ProductList = [
 "Product": "BISKUAT SWAVORY"
 }];
 
-APIdata = {
-    "payload": {
-      "row": {
-        "values": [
-          "NOEVENT",
-          "2017-03-01",
-          "502005",
-          "NOEVENT",
-          "WARUNG",
-          "CHEDDDAR"
-        ],
-        "columnSpecIds": [
-          "8995497152486572032",
-          "6113193390969454592",
-          "5536732638666031104",
-          "7842575647879725056",
-          "8419036400183148544",
-          "925046620238643200"
-        ]
-      }
+APIdata ={
+  "instances":[
+    {
+      "MONTH": "2020-11-01",
+      "Distributor": "100000",
+      "Product": "BISKUAT JUMBO",
+      "OutletType": "SUPERMARKET",
+      "CountryFeatures": "NOEVENT",
+      "ProductFeatures": "NOEVENT"
+    },
+    {
+      "MONTH": "2020-08-01",
+      "Distributor": "100000",
+      "Product": "CHIPS AHOY MEDIUM",
+      "OutletType": "SUPERMARKET",
+      "CountryFeatures": "NOEVENT",
+      "ProductFeatures": "NOEVENT"
+    },
+    {
+      "MONTH": "2020-10-01",
+      "Distributor": "100000",
+      "Product": "CHIPS AHOY MEDIUM",
+      "OutletType": "SUPERMARKET",
+      "CountryFeatures": "NOEVENT",
+      "ProductFeatures": "NOEVENT"
     }
+  ]
   }
