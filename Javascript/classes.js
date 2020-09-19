1202772041236ba4fccd38b5aa53eb5b2a39a41d
Stewart Bell <stewbell@gmail.com>
2020-09-19 11:05:08 +1000

    Client Perf Test 1

--- Javascript/classes.js
+++ Javascript/classes.js
@@ -34,66 +34,85 @@ function loadClient() {
 
 class dataTables {
     constructor() {
-        console.log("constructor")
+        //console.log("constructor")
+        this.dataArray = [];
         // var dataTable = new google.visualization.DataTable();
     }
     loadBaseData(dataset) {
         //load base BQ tables
-        var data = new google.visualization.DataTable();
-        var dataArray = [];
+        //var data = new google.visualization.DataTable();
+       
         var fieldArray = [];
       
 
         //load headings
         var field;
 
-        for (field of dataset.result.schema.fields) {
-            console.log("field headings", field.name);
-            fieldArray.push(field.name)
-        }
+        //for (field of dataset.result.schema.fields) {
+        //    console.log("field headings", field.name);
+        //    fieldArray.push(field.name)
+        //}
 
-        data.addColumn('string', 'Month');
-        data.addColumn('string', 'Product');
-        data.addColumn('string', 'Outlet Type');
-        data.addColumn('string', 'Distributor');
+        //data.addColumn('string', 'Month');
+        //data.addColumn('string', 'Product');
+        //data.addColumn('string', 'Outlet Type');
+        //data.addColumn('string', 'Distributor');
         //CountryFeatures
         //ProductFeatures
-        data.addColumn('number', 'Forecast Baseline');
-        data.addColumn('number', 'Forecast Total');
+        //data.addColumn('number', 'Forecast Baseline');
+        //data.addColumn('number', 'Forecast Total');
 
 
         //load row data
-        console.log("dataset", dataset)
+        var arrayProcTimmer = new timmer("From BQ to array format 1mill recs")
         var row;
         for (row of dataset.result.rows) {
             var rowArray = [];
-            rowArray.push(row.f[0]);
-            rowArray.push(row.f[1]);
-            rowArray.push(row.f[2]);
-            rowArray.push(row.f[3]);
-            rowArray.push(parseInt(row.f[6].v));
-            rowArray.push(parseInt(row.f[7].v));
+            rowArray.push(row.f[0].v);
+            rowArray.push(row.f[1].v);
+            rowArray.push(row.f[2].v);
+            rowArray.push(row.f[3].v);
+            rowArray.push(parseFloat(row.f[6].v));
+            rowArray.push(parseFloat(row.f[7].v));
 
-            dataArray.push(rowArray)
+            this.dataArray.push(rowArray)
         }
-        console.log("data Array", dataArray)
+        arrayProcTimmer.lapTime("Complete")
 
-        var options = {
-            title: 'Company Performance',
-            //curveType: 'function',
-            legend: { position: 'bottom' }
-        };
-            data.addRows(dataArray);
-
-
-
-
+        return(this)
+//        var options = {
+//            title: 'Company Performance',
+//            //curveType: 'function',
+//            legend: { position: 'bottom' }
+//        };
+//            data.addRows(dataArray);
         //var chart = new google.visualization.LineChart(document.getElementById(this.elementID));
         //chart.draw(data, options);
     }
+    AggByProduct() {
+        var procTime = new timmer('Aggregate Array');
+        var row;
+        var sumArray =[];
+        for (row of this.dataArray){
+            var key = [row[0],row[1]].join('$$');
+            sumArray[key] = (sumArray[key] === 
+                undefined) ? [row[4], row[5]] : [sumArray[key][0] + row[4], sumArray[key][1] + row[5]];
+        }
+        procTime.lapTime('Summarise');
+        var tempDataset = [];
+        for (key in sumArray){
+            var a = key.split('$$')
+            var b = sumArray[key];
+            var c = [a,b].flat();           
+            tempDataset.push(c);
+        }
+        procTime.lapTime('Flatten');
+        var newDataset = new dataTables();
+        newDataset.dataArray = tempDataset;
+       
+        return (newDataset);  
+    }
 }
-
-
 class timmer {
     constructor(label){
         this.label=label
